73c72c31ebe38fde67e84658d13dee9a7583dc1d
budi <budihmesa@gmail.com>
2019-05-24 10:14:12 +0700

    new file

---
+++ controllers/Joy1Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+   
+    public function actionJoy1()
+    {
+
+        echo "hello 1";
+    }
+}
+
